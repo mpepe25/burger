e9b6dd580e3565527ecfedf33f371214374a26e3
Marie  Pepe <mariepepe@Maries-MacBook-Air.local>
2019-08-20 16:17:40 -0400

    update

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,23 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    port: 3306,
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
+    port: 3306,
     user: "root",
     password: "password",
     database: "burgers_db"
-});
+  });
+}
 
-
-connection.connect(function (err) {
-    if (err) {
-        console.log("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+connection.config.typeCast = function (field, next) {
+  if (field.type == "TINY" && field.length == 1) {
+    return field.string() == "1"; // 1 = true, 0 = false
+  }
+  return next();
+};
 
 module.exports = connection;
